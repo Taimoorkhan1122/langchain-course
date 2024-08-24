13af04fb83a7ad798afd64d1db1d138fbdab2765
Taimoor Khan <khant8k83@gmail.com>
2024-08-24 18:10:38 +0500

    feat: vector retriever

--- src/3_vectors.ts
+++ src/3_vectors.ts
@@ -5,27 +5,34 @@ import { OpenAIEmbeddings } from "@langchain/openai";
 import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
 import { MemoryVectorStore } from "langchain/vectorstores/memory";
 
+// create an embedding
+const embeddings = new OpenAIEmbeddings();
+// const query = await embeddings.embedQuery("what is cyber security?");
 
-  // create an embedding
-  const embeddings = new OpenAIEmbeddings();
-  // const query = await embeddings.embedQuery("what is cyber security?");
+// load the document
 
-  // load the document
+const pdfLoader = new PDFLoader("./docs/machineLearning-lecture01.pdf");
+const doc = await pdfLoader.load();
 
-  const pdfLoader = new PDFLoader("./docs/machineLearning-lecture01.pdf");
-  const doc = await pdfLoader.load();
+// initiate the splitter and split the doc to fit the context window of LLM
+const splitter = new RecursiveCharacterTextSplitter({
+  chunkSize: 128,
+  chunkOverlap: 0,
+});
+const splitDoc = await splitter.splitDocuments(doc);
 
-  // initiate the splitter and split the doc to fit the context window of LLM
-  const splitter = new RecursiveCharacterTextSplitter({
-    chunkSize: 128,
-    chunkOverlap: 0,
-  });
-  const splitDoc = await splitter.splitDocuments(doc);
+// // load in memory vector store
+const vectorStore = new MemoryVectorStore(embeddings);
+await vectorStore.addDocuments(splitDoc);
 
-  // // load in memory vector store
-  const vectorStore = new MemoryVectorStore(embeddings);
-  await vectorStore.addDocuments(splitDoc);
+// we can retrieve doc using similarity search
+// const retrievedDocs = await vectorStore.similaritySearch(
+//   "what is the topic of the lecture?",
+//   4
+// );
 
-  const retrievedDocs = await vectorStore.similaritySearch("what is the topic of the lecture?", 4);
+// or langchain provides interface to return docs as retriever which Expression language chainable
+const retriever = vectorStore.asRetriever()
+const retrievedDocs = await retriever.invoke('what is machine learning?')
 
-  console.log(retrievedDocs.map(d => d.pageContent));
+console.log(retrievedDocs.map((d) => d.pageContent));
